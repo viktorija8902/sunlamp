1dd1272d800a241bd7e82d961b06664962271a7a
Viktorija <viktorija.bezsazna@gmail.com>
2019-01-13 00:00:40 -0500

    Set/get schedules

--- App.js
+++ App.js
@@ -20,95 +20,251 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
-    state = {
-    day: '',
-    hour: '',
-    minute: '',
+  state = {
+        workdayHour: "",
+        workdayMinute: "",
+        holidayHour: "",
+        holidayMinute: "",
+        showSuccessIcon: false,
+        turnedOff: false,
+        error: false,
+        connectionStatus: "connecting",
   };
 
+  workdaysBinary = "10111110";
+  workdayBitmask = parseInt(this.workdaysBinary, 2);
+  holidayBinary = "11000001";
+  holidayBitmask = parseInt(this.holidayBinary, 2);
+  maxBrightness = 255;
+  minBrightness = 0;
+  brightenPerSecond = 0.10;
+  darkenPerSecond = 10;
+  numberOfSchedules = 2; //turn-off and turn-on schedule
+
   componentDidMount() {
-    console.log("mounted")
-    BluetoothSerial.on('read', (data) => {
-      console.log('Reading data: ', data)
+    // console.log("mounted")
+    BluetoothSerial.withDelimiter('\n').then(() => {
+        this.handleResponses();
+    });
+
+    return this.connectToDevice().then(connected => {
+          if (connected) {
+            console.log("getting schedules")
+            BluetoothSerial.write("gsc\n")
+          }
+    });
+  }
+
+  componentWillUnMount() {
+    BluetoothSerial.disconnect();
+  }
+
+  handleResponses = () => {
+    BluetoothSerial.on('read', (response) => {
+      console.log('Reading data: ', response)
+      const responseArray = response.data.split(" ");
+      const schedulesReceived = responseArray[0] === "ACK" && responseArray.length > 2;
+      const lampTurnedOff = "ACK 0\r\n";
+      const scheduleSet = responseArray[0] === "ACK\r\n" && responseArray.length === 1;
+      const error = responseArray[0].startsWith("ERR");
+      if (error) {
+        this.setState({
+          error: true,
+          turnedOff: false,
+          showSuccessIcon: false,
+        });
+      }
+      else if (scheduleSet) {
+        this.setState({
+            error: false,
+            turnedOff: false,
+            showSuccessIcon: true,
+        });
+      } else if (responseArray[0] === lampTurnedOff) {
+          this.setState({
+            error: false,
+            turnedOff: true,
+            showSuccessIcon: false,
+          });
+      } else if (schedulesReceived) {
+        if (responseArray[2] === this.workdayBitmask.toString()) {
+          this.setState({
+            workdayHour: parseInt(responseArray[3]),
+            workdayMinute: parseInt(responseArray[4]),
+          });
+        } else if (responseArray[2] === this.holidayBitmask.toString()) {
+          this.setState({
+            holidayHour: parseInt(responseArray[3]),
+            holidayMinute: parseInt(responseArray[4]),
+          });
+        }
+      }
     })
   }
+
+   connectToDevice = () => {
+      return BluetoothSerial.list()
+         .then(data => {
+            console.log("devices", data)
+            const id = data.find(device => device.name === "SUNLAMP").id;
+            return BluetoothSerial.connect(id)
+         }).then(()=> {
+              this.setState({
+                 connectionStatus: "connected"
+              })
+              return true;
+         }).catch(error => {
+              console.error("connection error", error)
+              this.setState({
+                 connectionStatus: "not-connected"
+              })
+              return false;
+         })
+    }
+
 
   renderOptionsFrom = options =>
     options.map(option => <Picker.Item key={option.toString()} label={option.toString()} value={option} />);
 
   renderDefaultValue = value => <Picker.Item label={`Select ${value}`} value="not selected"/>;
 
-  getSchedules = () => {
+  turnOff = () => {
     console.log("CLICKED")
-    return BluetoothSerial.list()
-         .then(data => {
-            console.log("data", data)
-            const id = data.find(device => device.name === "SUNLAMP").id;
-            return BluetoothSerial.connect(id)
-        }).then(() => {
-            BluetoothSerial.write("sdl 0\n")
-            console.log("written")
-        });
+    return BluetoothSerial.isConnected()
+      .then(connected => {
+        if (connected) {
+          BluetoothSerial.write("sdl 0\n");
+        } else {
+          return this.connectToDevice().then(connected => {
+            if (connected) {
+              BluetoothSerial.write("sdl 0\n");
+            }
+          })
+        }
+      });
   }
+
+  setSchedule = () => {
+    return BluetoothSerial.isConnected()
+        .then(connected => {
+            if (connected) {
+                this.sendWorkdaySchedule();
+                this.sendHolidaySchedule();
+            } else {
+                return this.connectToDevice().then(connected => {
+                   if (connected) {
+                        this.sendWorkdaySchedule();
+                        this.sendHolidaySchedule();
+                   }
+                });
+            }
+        })
+  }
+
+  sendWorkdaySchedule() {
+     const workdayTurnOnSchedule = `${this.state.workdayHour} ${this.state.workdayMinute} ${this.maxBrightness} ${this.brightenPerSecond}`;
+     const workdayTurnOffSchedule = `${this.getTurnOffHour(this.state.workdayHour)} ${this.state.workdayMinute} ${this.minBrightness} ${this.darkenPerSecond}`;
+     BluetoothSerial.write(`ssc ${this.numberOfSchedules} ${this.workdayBitmask} ${workdayTurnOnSchedule} ${this.workdayBitmask} ${workdayTurnOffSchedule}\n`);
+  }
+
+  sendHolidaySchedule() {
+     const holidayTurnOnSchedule = `${this.state.holidayHour} ${this.state.holidayMinute} ${this.maxBrightness} ${this.brightenPerSecond}`;
+     const holidayTurnOffSchedule = `${this.getTurnOffHour(this.state.holidayHour)} ${this.state.holidayMinute} ${this.minBrightness} ${this.darkenPerSecond}`;
+     BluetoothSerial.write(`ssc ${this.numberOfSchedules} ${this.holidayBitmask} ${holidayTurnOnSchedule} ${this.holidayBitmask} ${holidayTurnOffSchedule}\n`);
+  }
+
+  getTurnOffHour = hour => {
+    if (hour === 23) {
+        return 0;
+    } else {
+        return hour + 1;
+    }
+  }
+
   render() {
-    const days = [
-      'monday',
-      'tuesday',
-      'wednesday',
-      'thursday',
-      'friday',
-      'saturday',
-      'sunday',
-    ];
-    const hours = [...Array(24).keys()];
-    const minutes = [...Array(60).keys()];
+    if (this.state.connectionStatus === "connecting") {
+      return <View style={styles.container}>
+        <Text style={styles.welcome}>Connecting...</Text>
+      </View>
+    }
+    if (this.state.connectionStatus === "not-connected") {
+          return <View style={styles.container}>
+            <Text style={styles.welcome}>Failed to connect!</Text>
+            <Button
+              onPress={this.connectToDevice}
+              title="Try again"
+              color="#841584"
+              accessibilityLabel="Try connecting again"
+            />
+          </View>
+    }
+
+    const hours = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];//[...Array(24).fill().keys()];
+    const minutes = [0,5,10,15,20,25,30,35,40,45,50,55];//...Array(60).fill().keys()];
+    const minuteOptions = this.renderOptionsFrom(minutes);
+    const hoursOptions = this.renderOptionsFrom(hours);
+    const hourDefault = this.renderDefaultValue("hour")
+    const minuteDefault = this.renderDefaultValue("minute")
 
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Welcome to Lamp Control!</Text>
         <Button
-          onPress={this.getSchedules}
-          title="Get schedules"
+          onPress={this.turnOff}
+          title="Turn Off Lamp"
           color="#841584"
           accessibilityLabel="Learn more about this purple button"
         />
+
+        {this.state.showSuccessIcon && <Text style={styles.welcome}>Set!</Text>}
+        {this.state.error && <Text style={styles.welcome}>Error!</Text>}
+        {this.state.turnedOff && <Text style={styles.welcome}>Turned Off!</Text>}
+        <Text>Workday schedule</Text>
         <Picker
-          selectedValue={this.state.day}
+          selectedValue={this.state.workdayHour}
           mode="dropdown"
           style={styles.picker}
-          onValueChange={(itemValue, itemIndex) =>
-            this.setState({ day: itemValue })
-          }>
-          {this.renderDefaultValue("day")}
-          {this.renderOptionsFrom(days)}
-        </Picker>
-        <Picker
-          selectedValue={this.state.hour}
-          mode="dropdown"
-          style={styles.picker}
-          onValueChange={(itemValue, itemIndex) =>
-            this.setState({ hour: itemValue })
-          }>
-          {this.renderDefaultValue("hour")}
-          {this.renderOptionsFrom(hours)}
+          onValueChange={itemValue => this.setState({ workdayHour: itemValue })}
+          >
+          {hourDefault}
+          {hoursOptions}
         </Picker>
          <Picker
-          selectedValue={this.state.minute}
+          selectedValue={this.state.workdayMinute}
           mode="dropdown"
           style={styles.picker}
-          onValueChange={(itemValue, itemIndex) =>
-            this.setState({ minute: itemValue })
-          }>
-          {this.renderDefaultValue("minute")}
-          {this.renderOptionsFrom(minutes)}
+          onValueChange={itemValue => this.setState({ workdayMinute: itemValue })}
+          >
+          {minuteDefault}
+          {minuteOptions}
         </Picker>
+
+        <Text>Holiday schedule</Text>
+        <Picker
+          selectedValue={this.state.holidayHour}
+          mode="dropdown"
+          style={styles.picker}
+          onValueChange={itemValue => this.setState({ holidayHour: itemValue })}
+          >
+          {hourDefault}
+          {hoursOptions}
+        </Picker>
+         <Picker
+          selectedValue={this.state.holidayMinute}
+          mode="dropdown"
+          style={styles.picker}
+          onValueChange={itemValue => this.setState({ holidayMinute: itemValue })}
+          >
+          {minuteDefault}
+          {minuteOptions}
+        </Picker>
+
+
         <Button
-          onPress={() => this.setState({hour: "", minute: "", day:""})}
-          title="Reset"
+          onPress={this.setSchedule}
+          title="Set schedule"
           color="#841584"
-          accessibilityLabel="Learn more about this purple button"
+          accessibilityLabel="Set schedule"
         />
       </View>
     );
